4ec7e5a2671528f8c07c34d4ddf1f16d794a3f95
正锐 <2290715547@qq.com>
2022-08-14 23:20:01 +0800

    fix: 换行符

--- config/unocss.ts
+++ config/unocss.ts
@@ -1,5 +1,5 @@
 /*
- * @Descripttion: 
+ * @Descripttion:
  * @Author: BZR
  * @Date: 2022-08-12 09:46:40
  * @LastEditTime: 2022-08-12 11:14:27
@@ -8,51 +8,51 @@ import { presetUno, presetAttributify, presetIcons } from "unocss";
 import Unocss from "unocss/vite";
 
 const colors = [
-    "white",
-    "black",
-    "gray",
-    "red",
-    "yellow",
-    "green",
-    "blue",
-    "indigo",
-    "purple",
-    "pink",
+  "white",
+  "black",
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
 ];
 
 const icons = [
-    "search",
-    "edit",
-    "check",
-    "message",
-    "star-off",
-    "delete",
-    "add",
-    "share",
-]
+  "search",
+  "edit",
+  "check",
+  "message",
+  "star-off",
+  "delete",
+  "add",
+  "share",
+];
 
 const safelist = [
-    ...colors.map((v) => `bg-${v}-100`),
-    ...colors.map((v) => `bg-${v}-400`),
-    ...colors.map((v) => `bg-${v}-500`),
-    ...colors.map((v) => `hover:bg-${v}-100`),
-    ...colors.map((v) => `hover:bg-${v}-300`),
-    ...colors.map((v) => `hover:bg-${v}-400`),
-    ...colors.map((v) => `hover:bg-${v}-500`),
-    ...colors.map((v) => `border-${v}-400`),
-    ...colors.map((v) => `border-${v}-500`),
-    ...colors.map((v) => `text-${v}-500`),
-    ...colors.map((v) => `hover:text-${v}-500`),
+  ...colors.map((v) => `bg-${v}-100`),
+  ...colors.map((v) => `bg-${v}-400`),
+  ...colors.map((v) => `bg-${v}-500`),
+  ...colors.map((v) => `hover:bg-${v}-100`),
+  ...colors.map((v) => `hover:bg-${v}-300`),
+  ...colors.map((v) => `hover:bg-${v}-400`),
+  ...colors.map((v) => `hover:bg-${v}-500`),
+  ...colors.map((v) => `border-${v}-400`),
+  ...colors.map((v) => `border-${v}-500`),
+  ...colors.map((v) => `text-${v}-500`),
+  ...colors.map((v) => `hover:text-${v}-500`),
 
-    ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
-    ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
-    ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
-    ...["rounded-full", "rounded-lg"],
-    ...icons.map((v) => `i-ic-baseline-${v}`),
+  ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
+  ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
+  ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
+  ...["rounded-full", "rounded-lg"],
+  ...icons.map((v) => `i-ic-baseline-${v}`),
 ];
 
 export default () =>
-    Unocss({
-        safelist,
-        presets: [presetUno(), presetAttributify(), presetIcons()],
-    });
+  Unocss({
+    safelist,
+    presets: [presetUno(), presetAttributify(), presetIcons()],
+  });
