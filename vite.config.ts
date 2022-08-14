4d9ff80b8f9087d54077bf6eb230027eab250394
正锐 <2290715547@qq.com>
2022-08-14 23:15:57 +0800

    feat: workflows

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,10 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), Unocss()],
   build: {
     rollupOptions,
-    minify: false,
+    minify: "terser",
+    sourcemap: true,
+    reportCompressedSize: true,
+    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "ZzsUI",
